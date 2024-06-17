44c78b95aa31f390023aacc71e90811dcd5ee759
Felix VANDAMME <f.vandamme@divy-si.fr>
2024-06-17 13:30:29 +0200

    notfoundview

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import NotFoundView from '../views/NotFoundView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -32,6 +33,11 @@ const router = createRouter({
       props: true,
       name: 'project',
       component: () => import('../views/ProjectView.vue')
+    },
+    {
+      path: '/:pathMatch(.*)*',
+      name: 'NotFound',
+      component: NotFoundView
     }
   ]
 })
---
+++ src/views/NotFoundView.vue
@@ -0,0 +1,35 @@
+<template>
+    <div id="erreur">
+        <h1>Oh non ! Vous êtes perdus ?</h1>
+        <RouterLink :to="{ name: 'home'}">Pour retourner sur l'accueil c'est par ici.</RouterLink>
+    </div>
+    <img src="https://media.istockphoto.com/id/1023347350/fr/photo/point-dinterrogation-3d-point-rouge-dinterrogation-demandant-signe-de-ponctuation-isol%C3%A9e.jpg?s=612x612&w=0&k=20&c=eVZrCH5I73a5W_2TZ0tlrWdK68UAoXZPaytoZyGoj90="/>
+</template>
+
+<style scoped>
+h1{
+    color:red;
+}
+img{
+    width: 500px;
+    animation:rotate 3s infinite linear;
+}
+
+#erreur{
+    flex:1;
+    flex-direction: column;
+    margin-bottom: 100px;
+}
+@keyframes rotate{
+    from{
+        transform:rotate(-20deg)
+    }
+    50%{
+        transform:rotate(20deg)
+    }
+    to{
+        transform:rotate(-20deg)
+    }
+}
+
+</style>
